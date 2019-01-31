d547763a7729f88115a1bb8254bfad9ba2c24c51
ajhsu <im.ajhsu@gmail.com>
2019-01-31 18:25:57 +0800

    Reformat with Prettier

--- example.js
+++ example.js
@@ -2,28 +2,30 @@ const noble = require('noble');
 const XiaomiServiceReader = require('./index');
 
 noble.on('stateChange', function(state) {
-	if (state === 'poweredOn') {
-		noble.startScanning([], true);
-	} else {
-		noble.stopScanning();
-	}
+  if (state === 'poweredOn') {
+    noble.startScanning([], true);
+  } else {
+    noble.stopScanning();
+  }
 });
 
 noble.on('discover', function(peripheral) {
-	const {advertisement, id, rssi, address} = peripheral;
-	const {localName, serviceData, serviceUuids} = advertisement;
-	let xiaomiData = null;
+  const { id, address, addressType, connectable, rssi, advertisement } = peripheral;
+  const { localName, serviceData, serviceUuids } = advertisement;
+  let xiaomiData = null;
+  for (let i in serviceData) {
+    if (serviceData[i].uuid.toString('hex') === 'fe95') {
+      xiaomiData = serviceData[i].data;
+    }
+  }
 
-	for (let i in serviceData) {
-		if (serviceData[i].uuid.toString('hex') === 'fe95') {
-			xiaomiData = serviceData[i].data;
-		}
-	}
+  if (!xiaomiData) return;
 
-	if (!xiaomiData) return;
-
-	console.log({
-		id, address, localName, rssi,
-		data: JSON.stringify(XiaomiServiceReader.readServiceData(xiaomiData)),
-	})
+  console.log({
+    id,
+    address,
+    localName,
+    rssi,
+    data: JSON.stringify(XiaomiServiceReader.readServiceData(xiaomiData))
+  });
 });
